6fe336e6c68b22e8f0a808386e0563dd377ba8c9
Vincent Baaij <vnbaaij@hotmail.com>
2013-11-28 11:38:30 +0100

    Initial commit

---
+++ App_Code/FolderTreeController.cs
@@ -0,0 +1,88 @@
+﻿using System.IO;
+using System.Linq;
+using System.Net.Http.Formatting;
+
+using Umbraco.Core;
+using Umbraco.Core.IO;
+using Umbraco.Web.Models.Trees;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.Trees;
+
+
+/// <summary>
+/// Shows tree of all folders in the site's filesystem or files in a selected folder
+/// </summary>
+
+//add treecontroller
+[Tree("settings", "diskTree", "Folders")]
+[PluginController("DiskPicker")]
+public class FolderTreeController : TreeController
+{
+
+
+    protected override TreeNodeCollection GetTreeNodes(string id, FormDataCollection queryStrings)
+    {
+        if (string.IsNullOrWhiteSpace(queryStrings.Get("files")))
+        {
+            //check if we're rendering the root node's children
+            if (id == Constants.System.Root.ToInvariantString())
+            {
+                return AddFolders("", queryStrings);
+            }
+            return AddFolders(id, queryStrings);
+        }
+        else
+        {
+            return AddFiles(queryStrings);
+        }
+
+    }
+
+    private TreeNodeCollection AddFiles(FormDataCollection queryStrings)
+    {
+        if (string.IsNullOrWhiteSpace(queryStrings.Get("startfolder")))
+            return null;
+
+        string path = IOHelper.MapPath(queryStrings.Get("startfolder"));
+
+        var nodes = new TreeNodeCollection();
+        DirectoryInfo folder = new DirectoryInfo(path);
+
+        foreach (var file in folder.EnumerateFiles())
+        {
+            var node = CreateTreeNode(file.FullName.Replace(path, "").Replace("\\", "/"), path, queryStrings, file.Name, "icon-document", false);
+            nodes.Add(node);
+        }
+        return nodes;
+    }
+
+
+    private TreeNodeCollection AddFolders(string parent, FormDataCollection queryStrings)
+    {
+        string relPath = "~/" + parent;
+        string absPath = IOHelper.MapPath(relPath);
+        
+        var nodes = new TreeNodeCollection();
+
+        DirectoryInfo dirs = new DirectoryInfo(absPath);
+               
+        foreach (var dir in dirs.EnumerateDirectories())
+        {
+            var node = CreateTreeNode(dir.FullName.Replace(IOHelper.MapPath("~"), "").Replace("\\", "/"), relPath, queryStrings, dir.Name, "icon-folder", dir.EnumerateDirectories().Any());
+            nodes.Add(node);
+        }
+        
+        return nodes;
+    }
+
+
+
+    protected override MenuItemCollection GetMenuForNode(string id, FormDataCollection queryStrings)
+    {
+
+        var menu = new MenuItemCollection();
+
+        return null;
+
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,24 @@ umbraco7-filepicker
 ===================
 
 File Picker Property Editor for Umbraco V7
+
+Installation
+====
+
+1. Drop the folders from this repository into the corresponding folders of an Umbraco V7 instance.
+2. Create a new data type of type "File Picker" in the "Developer" section in the Umbraco backoffice.
+3. Choose "Pick Folder" to select the folder in the Umbraco V7 instance from which a file can be selected in the document
+4. Assign the data type to a document type in the "Settings" section of the Umbraco backoffice.
+5. You can now select a file in a document of the type you altered in step 4
+
+
+Disclaimers
+====
+
+This is just something I've build to see if I could do it. There will probably be errors.
+
+
+Contact me
+====
+
+If you have tips or questions, give me a shout on twitter [@vnbaaij](http://www.twitter.com/vnbaaij).
