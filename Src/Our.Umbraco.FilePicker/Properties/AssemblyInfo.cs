c875c6b573adc969801ae6baeb77e9ad8f616f8c
Vincent Baaij <vnbaaij@outlook.com>
2014-11-22 20:12:26 +0100

    V1.2 for real now

--- Src/Our.Umbraco.FilePicker/Properties/AssemblyInfo.cs
+++ Src/Our.Umbraco.FilePicker/Properties/AssemblyInfo.cs
@@ -4,14 +4,14 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Our.Umbraco.FilePicker")]
 [assembly: AssemblyDescription("A File Picker property editor for Umbraco 7")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Umbrella Inc Ltd")]
+[assembly: AssemblyCompany("Vincent Baaij, Lee Kelleher, Matt Brailsford")]
 [assembly: AssemblyProduct("Our.Umbraco.FilePicker")]
-[assembly: AssemblyCopyright("Copyright \xa9 Umbrella Inc Ltd 2014")]
+[assembly: AssemblyCopyright("Copyright \xa9 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("35B141D2-D002-4CDA-AAD3-85808977C850")]
 
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
